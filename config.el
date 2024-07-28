57a6cb88858c30b4522a9f69a7ce0f4005fc2eac
tkiet002 <tuankietgam2505@gmail.com>
2024-07-28 22:49:50 +0700

    Change and add more picture

--- config.el
+++ config.el
@@ -35,10 +35,10 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox-light)
+(setq doom-theme 'dichromacy)
 
 
-(setq fancy-splash-image (concat doom-user-dir "villv.png"))
+(setq fancy-splash-image (concat doom-user-dir "villv2.jpeg"))
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
